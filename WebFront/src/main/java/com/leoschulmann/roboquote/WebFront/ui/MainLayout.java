871062f896bd52cebadc5a6b8baa16efc51bc126
levchigintsev <lev.chigintsev@gmail.com>
2020-11-11 21:46:51 +0300

    simple application layout

---
+++ WebFront/src/main/java/com/leoschulmann/roboquote/WebFront/ui/MainLayout.java
@@ -0,0 +1,27 @@
+package com.leoschulmann.roboquote.WebFront.ui;
+
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.router.HighlightConditions;
+import com.vaadin.flow.router.RouterLink;
+
+public class MainLayout extends AppLayout {
+    public MainLayout() {
+        H1 logo = new H1("Roboquote");
+        logo.addClassName("logo");
+        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo);
+        header.setVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.setWidth("100%");
+        header.addClassName("header");
+        addToNavbar(header);
+
+        RouterLink linkToCompose = new RouterLink("Compose", Compose.class);
+        RouterLink lingToInventory = new RouterLink("Inventory", InventoryView.class);
+        linkToCompose.setHighlightCondition(HighlightConditions.sameLocation());
+        lingToInventory.setHighlightCondition(HighlightConditions.sameLocation());
+        addToDrawer(linkToCompose, lingToInventory);
+    }
+}
