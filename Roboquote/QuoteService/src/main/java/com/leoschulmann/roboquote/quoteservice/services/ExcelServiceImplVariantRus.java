4b366f4cd5174a51827c9b836beee32c46bf64c9
levchigintsev <lev.chigintsev@gmail.com>
2020-11-23 23:51:35 +0300

    .xlsx export (mostly done)

--- Roboquote/QuoteService/src/main/java/com/leoschulmann/roboquote/quoteservice/services/ExcelServiceImplVariantRus.java
+++ Roboquote/QuoteService/src/main/java/com/leoschulmann/roboquote/quoteservice/services/ExcelServiceImplVariantRus.java
@@ -1,61 +1,59 @@
 package com.leoschulmann.roboquote.quoteservice.services;
 
+import com.leoschulmann.roboquote.quoteservice.entities.ItemPosition;
 import com.leoschulmann.roboquote.quoteservice.entities.Quote;
+import com.leoschulmann.roboquote.quoteservice.entities.QuoteSection;
 import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.ss.util.RegionUtil;
 import org.apache.poi.util.IOUtils;
 import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.javamoney.moneta.format.CurrencyStyle;
 import org.springframework.stereotype.Service;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import javax.annotation.PostConstruct;
+import javax.money.MonetaryAmount;
+import javax.money.format.AmountFormatQueryBuilder;
+import javax.money.format.MonetaryAmountFormat;
+import javax.money.format.MonetaryFormats;
+import java.io.*;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.IntStream;
 
 @Service
 public class ExcelServiceImplVariantRus implements ExcelService {
+    private Sheet sheet;
+    private Workbook workbook;
+    private MonetaryAmountFormat formatter;
+
+    @PostConstruct
+    private void init() {
+        formatter = MonetaryFormats.getAmountFormat(
+                AmountFormatQueryBuilder
+                        .of(Locale.FRANCE)
+                        .set(CurrencyStyle.SYMBOL)
+                        .build());
+    }
+
     @Override
     public void generateFile(Quote quote, String fileLocation) {
-        Workbook workbook = new XSSFWorkbook();
-        Sheet sheet = workbook.createSheet(quote.getNumber());
+
+        workbook = new XSSFWorkbook();
+        sheet = workbook.createSheet(quote.getNumber());
         sheet.setColumnWidth(0, 19400); //75
         sheet.setColumnWidth(1, 1907);  //6.67
         sheet.setColumnWidth(2, 4252);  //15,83
         sheet.setColumnWidth(3, 3572);  //13.17
         sheet.setColumnWidth(4, 4852);  //18.17
 
-        try {
-            InputStream fileStream = new FileInputStream(ExcelServiceImplVariantRus.class.getClassLoader().getResource("pholder.png").getFile());
-            byte[] bytes = IOUtils.toByteArray(fileStream);
-            int pictureId = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
-            fileStream.close();
-            Drawing<?> drawing = sheet.createDrawingPatriarch();
-            XSSFClientAnchor anchor = new XSSFClientAnchor();
-            anchor.setCol1(0);
-            anchor.setRow1(0);
-            Picture picture = drawing.createPicture(anchor, pictureId);
-            picture.resize();
+        addPicture(ExcelServiceImplVariantRus.class.getClassLoader().getResource("pholder.png").getFile(), 0, 0);
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        XSSFFont titleFont = (XSSFFont) workbook.createFont();
-        titleFont.setBold(true);
-        titleFont.setFontHeight(11);
-        titleFont.setFontName("Calibri");
-
-        CellStyle topTitleStyle = workbook.createCellStyle();
-        topTitleStyle.setFont(titleFont);
-        topTitleStyle.setBorderBottom(BorderStyle.THIN);
-
-        CellStyle titleStyle = workbook.createCellStyle();
-        titleStyle.setFont(titleFont);
+        CellStyle topTitleStyle = getTopCellStyle(getXssfFont());
+        CellStyle titleStyle = getCellStyle(getXssfFont());
 
         for (int i = 0; i < 15; i++) {
             Row r = sheet.createRow(i);
@@ -66,7 +64,25 @@ public class ExcelServiceImplVariantRus implements ExcelService {
             }
         }
 
-        List<String> qDetails = List.of(
+        List<String> qDetails;
+        qDetails = getListOfDetails(quote);
+
+        IntStream.range(0, qDetails.size())
+                .forEach(i -> sheet.getRow(8 + i)
+                        .getCell(0)
+                        .setCellValue(qDetails.get(i)));
+
+
+        for (QuoteSection section : quote.getSections()) {
+            writeSection(section);
+        }
+
+        writeFile(fileLocation);
+    }
+
+    private List<String> getListOfDetails(Quote quote) {
+        List<String> qDetails;
+        qDetails = List.of(
                 new StringBuilder("Коммерческое предложение №")
                         .append(quote.getNumber())
                         .append(quote.getVersion())
@@ -84,14 +100,76 @@ public class ExcelServiceImplVariantRus implements ExcelService {
 
                 new StringBuilder("Срок поставки: ").append(quote.getShippingTerms()).toString()
         );
+        return qDetails;
+    }
+
+    private void addPicture(String file, int col1, int row1) {
+        try {
+            InputStream fileStream = new FileInputStream(file);
+            byte[] bytes = IOUtils.toByteArray(fileStream);
+            int pictureId = workbook.addPicture(bytes, Workbook.PICTURE_TYPE_PNG);
+            fileStream.close();
+            Drawing<?> drawing = sheet.createDrawingPatriarch();
+            XSSFClientAnchor anchor = new XSSFClientAnchor();
+            anchor.setCol1(col1);
+            anchor.setRow1(row1);
+            Picture picture = drawing.createPicture(anchor, pictureId);
+            picture.resize();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeSection(QuoteSection section) {
+        createBlankRows(3);
+        int subheaderRowNum = sheet.getLastRowNum() + 1;
+        Row subheaderRow = sheet.createRow(subheaderRowNum);
+        createBlankCells(subheaderRow, 5);
+        CellRangeAddress region = new CellRangeAddress(subheaderRowNum, subheaderRowNum, 0, 4);
+        sheet.addMergedRegion(region);
+        subheaderRow.getCell(0).setCellValue(section.getName());
+        RegionUtil.setBorderBottom(BorderStyle.THIN, region, sheet);
+        RegionUtil.setBorderTop(BorderStyle.MEDIUM, region, sheet);
+        subheaderRow.getCell(0).setCellStyle(getSubheaderStyle(getXssfFont()));
+        section.getPositions().forEach(this::writePosition);
+        drawWideLine();
+        addSubtotals(0, section.getName(), section.getTotal());
+        if (section.getDiscount() != null && section.getDiscount() > 0) {
+            addSubtotals(section.getDiscount(), section.getName(), section.getTotalDiscounted());
+        }
+    }
+
+    private void addSubtotals(int discount, String name, MonetaryAmount money) {
+        int idx = sheet.getLastRowNum() + 1;
+        Row row = sheet.createRow(idx);
+        createBlankCells(row, 5);
+        sheet.addMergedRegion(new CellRangeAddress(idx, idx, 0, 3));
+        String content = (discount > 0 ?
+                "ВСЕГО " + name + " (со скидкой " + discount + "%)" :
+                "ВСЕГО " + name);
+        row.getCell(0).setCellValue(content);
+        row.getCell(0).getCellStyle().setAlignment(HorizontalAlignment.RIGHT);
+        row.getCell(4).setCellValue(formatter.format(money));
+    }
 
 
-        IntStream.range(0, qDetails.size())
-                .forEach(i -> sheet.getRow(8 + i)
-                        .getCell(0)
-                        .setCellValue(qDetails.get(i)));
+    private void drawWideLine() {
+        Row r = sheet.getRow(sheet.getLastRowNum());
+        r.cellIterator().forEachRemaining(cell -> cell.setCellStyle(getLinedStyle()));
+    }
 
+    private void writePosition(ItemPosition pos) {
+        Row row = sheet.createRow(sheet.getLastRowNum() + 1);
+        createBlankCells(row, 5);
+        row.getCell(0).setCellValue(pos.getName());
+        row.getCell(1).setCellValue(pos.getQty());
+        row.getCell(2).setCellValue(pos.getPartNo());
+        row.getCell(3).setCellValue(formatter.format(pos.getSellingPrice()));
+        row.getCell(4).setCellValue(formatter.format(pos.getSellingSum()));
+    }
 
+    private void writeFile(String fileLocation) {
         FileOutputStream outputStream = null;
         try {
             outputStream = new FileOutputStream(fileLocation);
@@ -100,6 +178,50 @@ public class ExcelServiceImplVariantRus implements ExcelService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private CellStyle getCellStyle(XSSFFont titleFont) {
+        CellStyle titleStyle = workbook.createCellStyle();
+        titleStyle.setFont(titleFont);
+        return titleStyle;
+    }
+
+    private CellStyle getTopCellStyle(XSSFFont titleFont) {
+        CellStyle topTitleStyle = workbook.createCellStyle();
+        topTitleStyle.setFont(titleFont);
+        topTitleStyle.setBorderBottom(BorderStyle.THIN);
+        return topTitleStyle;
+    }
+
+    private CellStyle getSubheaderStyle(XSSFFont xssfFont) {
+        CellStyle subheaderStyle = workbook.createCellStyle();
+        subheaderStyle.setFont(xssfFont);
+        subheaderStyle.setAlignment(HorizontalAlignment.CENTER);
+        subheaderStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+        subheaderStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        return subheaderStyle;
+    }
+
+    private CellStyle getLinedStyle() {
+        CellStyle underline = workbook.createCellStyle();
+        underline.setBorderBottom(BorderStyle.MEDIUM);
+        return underline;
+    }
+
+    private XSSFFont getXssfFont() {
+        XSSFFont titleFont;
+        titleFont = (XSSFFont) workbook.createFont();
+        titleFont.setBold(true);
+        titleFont.setFontHeight(11);
+        titleFont.setFontName("Calibri");
+        return titleFont;
+    }
+
+    private void createBlankRows(int rows) {
+        IntStream.range(0, rows).forEach((i) -> sheet.createRow(sheet.getLastRowNum() + i));
+    }
+
+    private void createBlankCells(Row row, int cells) {
+        IntStream.range(0, cells).forEach(row::createCell);
     }
 }
--- Roboquote/QuoteService/src/test/java/com/leoschulmann/roboquote/quoteservice/test/ExcelFileTest.java
+++ Roboquote/QuoteService/src/test/java/com/leoschulmann/roboquote/quoteservice/test/ExcelFileTest.java
@@ -1,8 +1,11 @@
 package com.leoschulmann.roboquote.quoteservice.test;
 
+import com.leoschulmann.roboquote.quoteservice.entities.ItemPosition;
 import com.leoschulmann.roboquote.quoteservice.entities.Quote;
+import com.leoschulmann.roboquote.quoteservice.entities.QuoteSection;
 import com.leoschulmann.roboquote.quoteservice.services.ExcelService;
 import com.leoschulmann.roboquote.quoteservice.services.NameGeneratingService;
+import org.javamoney.moneta.Money;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -35,7 +38,22 @@ import static org.junit.jupiter.api.Assertions.*;
                 "Test customer",
                 "Test dealer",
                 "", "");
+        QuoteSection qs1 = new QuoteSection("Test section 1");
+        ItemPosition ip1 = new ItemPosition("Test item 1", "000", Money.of(100, "USD"),
+                1, -1);
+        ItemPosition ip2 = new ItemPosition("Test item 2", "001", Money.of(10, "USD"),
+                6, -1);
 
+        QuoteSection qs2 = new QuoteSection("Test section 2");
+        ItemPosition ip3 = new ItemPosition("Test item 3", "010", Money.of(15, "USD"),
+                10, -1);
+        ItemPosition ip4 = new ItemPosition("Test item 4", "011", Money.of(85, "USD"),
+                2, -1);
+
+        qs1.addItemPositions(ip1, ip2);
+        qs2.addItemPositions(ip3, ip4);
+        qs2.setDiscount(10);
+        quote.addSections(qs1, qs2);
     }
 
     @Test
