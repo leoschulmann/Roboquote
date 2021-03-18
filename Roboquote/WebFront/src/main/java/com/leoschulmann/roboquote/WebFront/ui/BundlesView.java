955dabcf0e3e2b92f078f6cf7eb3f42b84456890
levchigintsev <lev.chigintsev@gmail.com>
2021-03-18 23:40:57 +0300

    Bundles view cosmetics

--- Roboquote/WebFront/src/main/java/com/leoschulmann/roboquote/WebFront/ui/BundlesView.java
+++ Roboquote/WebFront/src/main/java/com/leoschulmann/roboquote/WebFront/ui/BundlesView.java
@@ -6,7 +6,12 @@ import com.leoschulmann.roboquote.itemservice.entities.Bundle;
 import com.leoschulmann.roboquote.itemservice.entities.BundledPosition;
 import com.leoschulmann.roboquote.itemservice.entities.Item;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.ColumnTextAlign;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.grid.GridVariant;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
@@ -22,12 +27,16 @@ import java.util.Optional;
 public class BundlesView extends VerticalLayout {
     private final TextField nameField;
     private final Grid<Item> availableItemsGrid;
+    private final Grid<BundledPosition> bundleItemsGrid;
     private Button addBtn;
-    private Button saveBundle;
+    private Button wrapBtn;
+    private Button saveBundleButton;
     private final ListDataProvider<Item> availableItemsDataProvider;
     private ListDataProvider<BundledPosition> selectedItemsDataProvider;
     private final Bundle bundle;
     private final BundleService bundleService;
+    private final Span counter = new Span("Total: ");
+    private boolean wrap = false;
 
 
     public BundlesView(ItemCachingService cachingService, BundleService bundleService) {
@@ -35,29 +44,93 @@ public class BundlesView extends VerticalLayout {
         availableItemsDataProvider = new ListDataProvider<>(cachingService.getItemsFromCache());
         this.bundleService = bundleService;
         availableItemsGrid = createAvailableGrid();
-        Grid<BundledPosition> bundleItemsGrid = createSelectedGrid();
+        bundleItemsGrid = createSelectedGrid();
         addBtn = createAddButton();
-        saveBundle = createSaveBundle();
-
+        wrapBtn = createWrapButton();
+        VerticalLayout middleButtons = new VerticalLayout(addBtn, wrapBtn);
+        middleButtons.setWidth("5%");
         nameField = new TextField("Bundle name");
-        nameField.setWidthFull();
+        saveBundleButton = createSaveBundle();
+        nameField.setWidth("90%");
+        saveBundleButton.setWidth("10%");
+        HorizontalLayout nameAndButton = new HorizontalLayout(nameField, saveBundleButton);
+        nameAndButton.setWidthFull();
+        nameAndButton.setAlignItems(Alignment.BASELINE);
+        add(nameAndButton);
+        HorizontalLayout mailPanel = new HorizontalLayout(availableItemsGrid, middleButtons, bundleItemsGrid);
+        mailPanel.setWidthFull();
+        mailPanel.setAlignItems(Alignment.CENTER);
+        add(mailPanel);
+    }
 
-        add(new HorizontalLayout(nameField, saveBundle));
-        add(new HorizontalLayout(availableItemsGrid, addBtn, bundleItemsGrid));
+    private Button createWrapButton() {
+        wrapBtn = new Button(VaadinIcon.LINES.create());
+        wrapBtn.addClickListener(c -> {
+            if (!wrap) {
+                bundleItemsGrid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
+                availableItemsGrid.addThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
+                wrap = true;
+            } else {
+                bundleItemsGrid.removeThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
+                availableItemsGrid.removeThemeVariants(GridVariant.LUMO_WRAP_CELL_CONTENT);
+                wrap = false;
+            }
+        });
+        return wrapBtn;
     }
 
     private Button createSaveBundle() {
-        saveBundle = new Button("SAVE");
-        saveBundle.addClickListener(c -> {
+        saveBundleButton = new Button("SAVE");
+        saveBundleButton.addThemeVariants(ButtonVariant.LUMO_SUCCESS, ButtonVariant.LUMO_PRIMARY);
+        saveBundleButton.addClickListener(c -> {
             bundle.setNameRus(nameField.getValue());
             bundle.setNameEng(nameField.getValue());
             bundleService.saveBundle(bundle);
         });
-        return saveBundle;
+        return saveBundleButton;
+    }
+
+    private Grid<BundledPosition> createSelectedGrid() {
+        final Grid<BundledPosition> bundleItemsGrid = new Grid<>(BundledPosition.class);
+        bundleItemsGrid.getStyle().set("font-size", "12px");
+
+        selectedItemsDataProvider = new ListDataProvider<>(bundle.getPositions());
+        bundleItemsGrid.setDataProvider(selectedItemsDataProvider);
+        bundleItemsGrid.removeAllColumns();
+
+        Grid.Column<BundledPosition> namecol = bundleItemsGrid.addColumn(bp -> bp.getItem().getNameRus());
+        Grid.Column<BundledPosition> qcol = bundleItemsGrid.addColumn(BundledPosition::getQty);
+        namecol.setHeader("Item name").setFlexGrow(1).setResizable(true);
+        qcol.setHeader("Qty").setFlexGrow(0).setTextAlign(ColumnTextAlign.CENTER).setFooter(counter);
+        bundleItemsGrid.addThemeVariants(GridVariant.LUMO_ROW_STRIPES, GridVariant.LUMO_COLUMN_BORDERS);
+
+        return bundleItemsGrid;
+    }
+
+    private Grid<Item> createAvailableGrid() {
+        final Grid<Item> availableItemsGrid = new Grid<>(Item.class);
+        availableItemsGrid.getStyle().set("font-size", "12px");
+
+        availableItemsGrid.removeAllColumns();
+        Grid.Column<Item> col = availableItemsGrid.addColumn(Item::getNameRus);
+
+        availableItemsGrid.setDataProvider(availableItemsDataProvider);
+
+        TextField filter = new TextField();
+        filter.setWidthFull();
+        filter.setPlaceholder("filter text");
+        filter.addThemeVariants(TextFieldVariant.LUMO_SMALL);
+        filter.setClearButtonVisible(true);
+        filter.addValueChangeListener(event -> availableItemsDataProvider.addFilter(
+                item -> StringUtils.containsIgnoreCase(item.getNameRus(), filter.getValue())));
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        col.setHeader(filter);
+        return availableItemsGrid;
     }
 
     private Button createAddButton() {
-        addBtn = new Button(">>");
+        addBtn = new Button(VaadinIcon.FORWARD.create());
+        addBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY, ButtonVariant.LUMO_SMALL);
         addBtn.addClickListener(c -> {
             Item item = availableItemsGrid.asSingleSelect().getValue();
             Optional<BundledPosition> opt = selectedItemsContainsItem(item);
@@ -70,40 +143,13 @@ public class BundlesView extends VerticalLayout {
                 bundle.addPosition(bp);
             }
             selectedItemsDataProvider.refreshAll();
+            counter.setText("Total : " + countItems());
         });
         return addBtn;
     }
 
-    private Grid<BundledPosition> createSelectedGrid() {
-        final Grid<BundledPosition> bundleItemsGrid;
-        bundleItemsGrid = new Grid<>(BundledPosition.class);
-        selectedItemsDataProvider = new ListDataProvider<>(bundle.getPositions());
-        bundleItemsGrid.setDataProvider(selectedItemsDataProvider);
-        bundleItemsGrid.removeAllColumns();
-        bundleItemsGrid.addColumn(bp -> bp.getItem().getNameRus());
-        bundleItemsGrid.addColumn(BundledPosition::getQty);
-        bundleItemsGrid.setWidth("30em");
-        return bundleItemsGrid;
-    }
-
-    private Grid<Item> createAvailableGrid() {
-        final Grid<Item> availableItemsGrid;
-        availableItemsGrid = new Grid<>(Item.class);
-        availableItemsGrid.removeAllColumns();
-        Grid.Column<Item> col = availableItemsGrid.addColumn(Item::getNameRus);
-        availableItemsGrid.setDataProvider(availableItemsDataProvider);
-        availableItemsGrid.setHeightByRows(true);
-        availableItemsGrid.setHeight("100em");
-        availableItemsGrid.setWidth("30em");
-
-        TextField filter = new TextField();
-        filter.addThemeVariants(TextFieldVariant.LUMO_SMALL);
-        filter.setClearButtonVisible(true);
-        filter.addValueChangeListener(event -> availableItemsDataProvider.addFilter(
-                item -> StringUtils.containsIgnoreCase(item.getNameRus(), filter.getValue())));
-        filter.setValueChangeMode(ValueChangeMode.EAGER);
-        col.setHeader(filter);
-        return availableItemsGrid;
+    private int countItems() {
+        return bundle.getPositions().stream().mapToInt(BundledPosition::getQty).sum();
     }
 
     Optional<BundledPosition> selectedItemsContainsItem(Item item) {
